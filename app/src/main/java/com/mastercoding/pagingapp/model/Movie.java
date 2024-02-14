2dd3217972b531feed14dd7f74ebd97fafdaf61f
codewithamaz <amaz8144@gmail.com>
2024-02-14 17:47:48 +0500

    Initial commit

---
+++ app/src/main/java/com/mastercoding/pagingapp/model/Movie.java
@@ -0,0 +1,65 @@
+package com.mastercoding.pagingapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+
+    @SerializedName("vote_average")
+    @Expose
+    private double voteAverage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( obj == null ) {
+            return false;
+        }
+        if ( obj == this ) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/mastercoding/pagingapp/util/Utils.java
@@ -0,0 +1,7 @@
+package com.mastercoding.pagingapp.util;
+
+public class Utils {
+    public static String API_KEY = "7fe6229c8579ebccfb425c945e486919";
+    public static String BASE_URL = "https://api.themoviedb.org/3/";
+
+}
